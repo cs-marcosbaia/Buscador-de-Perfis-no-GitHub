1e95a2b195d84c7358a370cf4e85c89ee7603455
MARCOS J S BAIA <marcosjsbaia@yahoo.com.br>
2022-09-24 00:55:46 -0300

    Trabalho concluido

---
+++ script.js
@@ -0,0 +1,62 @@
+const APIURL = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username){
+    try{
+        const { data } = await axios(APIURL + username)
+
+        createUserCard(data)
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('Usuário Inexistente!!!')
+        }
+    }
+}
+
+function createUserCard(user) {
+    const cardHTML = `            
+    <div class="card">
+        <div>
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+            
+            <ul>
+                <li>${user.followers} <strong>Seguidores</strong></li>
+                <li>${user.following} <strong>Seguindo</strong></li>
+                <li>${user.public_repos} <strong>Repositório</strong></li>
+            </ul>
+
+            <div id="repos"></div>
+        </div>
+    </div>
+    `
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+    
+    main.innerHTML = cardHTML
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
